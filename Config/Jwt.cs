bd4ca9a3c5d67bca73284bbee4de02e0ec4f7ec1
voduchuy2001 <voduchuy2001@gmail.com>
2025-03-30 16:23:06 +0700

    chores

--- Config/Jwt.cs
+++ Config/Jwt.cs
@@ -3,14 +3,21 @@ using System.Text;
 
 namespace Api.Config;
 
-public class Jwt(IConfiguration configuration)
+public class JWT
 {
-    public string SecretKey { get; } = configuration["JwtSettings:SecretKey"] ?? throw new ArgumentNullException("SecretKey is missing");
-    public string Issuer { get; } = configuration["JwtSettings:Issuer"] ?? throw new ArgumentNullException("Issuer is missing");
-    public string Audience { get; } = configuration["JwtSettings:Audience"] ?? throw new ArgumentNullException("Audience is missing");
+    public string SecretKey { get; }
+    public string Issuer { get; }
+    public string Audience { get; }
     public int Expiration { get; } = 60;
 
     public SymmetricSecurityKey SigningKey => new(Encoding.UTF8.GetBytes(SecretKey));
+
+    public JWT(IConfiguration configuration)
+    {
+        SecretKey = configuration["JwtSettings:SecretKey"] ?? throw new ArgumentNullException("SecretKey is missing");
+        Issuer = configuration["JwtSettings:Issuer"] ?? throw new ArgumentNullException("Issuer is missing");
+        Audience = configuration["JwtSettings:Audience"] ?? throw new ArgumentNullException("Audience is missing");
+    }
 
     public TokenValidationParameters GetTokenValidationParameters()
     {
